5e5d46d8fadc8e054ebae502ff320fbd03e52047
sunst <827489266@qq.com>
2015-04-27 15:23:10 +0800

    建库

---
+++ Xproject/js/app/uiElement/test.js
@@ -0,0 +1,6 @@
+xp.mod.define("app.uiElement.test",function(){
+	var module = {};
+	module.test = function(){
+		console.log("2222");
+	}
+})
---
+++ Xproject/js/xp/lang.js
@@ -0,0 +1,12 @@
+//提供一些工具函数
+var lang = {};
+lang = (function(){
+	var module = {};
+	//封装定时器
+	module.setInterval = function(){
+
+	};
+	module.clearInterval = function(){
+
+	};
+}(lang||{}))
